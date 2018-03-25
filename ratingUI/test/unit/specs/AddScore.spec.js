31bacde21cb5ecdd3985c75a775eecbf830bd26c
Ben Kielty <bwk103@gmail.com>
2018-03-25 10:23:52 +0100

    Create First Vue.js Component
    Created AddPost - which renders a form for user to create new post.

---
+++ ratingUI/test/unit/specs/AddScore.spec.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import AddScore from '@/components/AddScore'
+
+describe('AddScore.vue', () => {
+  it('renders the correct heading', () => {
+    const Constructor = Vue.extend(AddScore)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('.new-score h1').textContent)
+      .toEqual('Help us improve our website.')
+  })
+  it('renders the correct subheading', () => {
+    const Constructor = Vue.extend(AddScore)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('.new-score h3').textContent)
+      .toEqual('To help us to continually improve our service, please tell us how you rated your experience of the FT website.')
+  })
+})
