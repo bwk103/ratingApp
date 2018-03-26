e3534d75595898af28faff3f3b5a50479abd7b88
Ben Kielty <bwk103@gmail.com>
2018-03-26 10:29:14 +0100

    Refactor Routes
    Remove /feedback and /confirm routes which are not required due to
    front end SPA.  Refactored POST scores/new and GET /score to use
    async await.

--- ratingServer/routes/feedback.js
+++ ratingServer/routes/feedback.js
@@ -3,41 +3,48 @@ var router = require('express').Router();
 var Score = require('../models/score');
 
 router.get('/', (req, res) => {
-  res.send('Hello World!');
+  try {
+    res.send('Hello World!');
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-router.get('/feedback', (req, res) => {
-  res.send('The feedback page');
+router.post('/scores/new', async(req, res) => {
+  var score = new Score({ rating: req.body.rating, comment: req.body.comment });
+  try {
+    await score.save();
+    res.status(201).send({ status: 'Success',
+      message: 'Score saved successfully'});
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-router.post('/feedback', (req, res) => {
-  var score = new Score({
-    rating: req.body.rating,
-    comment: req.body.comment,
-  });
-  score.save((err, score) => {
-    if (err) {
-      console.error(err);
-    } else {
-      res.statusCode = 201;
-      res.send({
-        status: 'Success',
-        message: 'Score saved successfully',
-      });
-    }
-  });
+router.get('/scores', async(req, res) => {
+  try {
+    var allScores = await Score.find({});
+    res.send(allScores);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-router.get('/confirm', (req, res) => {
-  Score.find({}, function(err, scores){
-    if (err) {
-      console.error(err);
-    } else {
-      res.send({
-        message: 'Thanks for your feedback',
-      });
-    }
-  });
-});
+// router.get('/feedback', (req, res) => {
+//   try {
+//     res.send('The feedback page');
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
+
+// router.get('/confirm', async(req, res) => {
+//   try {
+//     // var allScores = await Score.find({});
+//     res.send({ message: 'Thanks for your feedback' });
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
 
 module.exports = router;
--- ratingServer/test/unit/routes.test.js
+++ ratingServer/test/unit/routes.test.js
@@ -12,41 +12,41 @@ afterAll(() => {
 });
 
 describe('routing tests', () => {
-  describe('/', () => {
-    test('responds with a 200 status code', () => {
-      request(app).get('/').then((response) => {
-        return expect(response.statusCode).toBe(200);
-      });
-    });
+  // describe('/', () => {
+  //   test('responds with a 200 status code', () => {
+  //     request(app).get('/').then((response) => {
+  //       return expect(response.statusCode).toBe(200);
+  //     });
+  //   });
+  //
+  //   test('responds with "Hello World" in the body', () => {
+  //     request(app).get('/').then((response) => {
+  //       return expect(response.text).toBe('Hello World!');
+  //     });
+  //   });
+  // });
 
-    test('responds with "Hello World" in the body', () => {
-      request(app).get('/').then((response) => {
-        return expect(response.text).toBe('Hello World!');
-      });
-    });
-  });
-
-  describe('/feedback', () => {
-    test('get requests respond with a 200 status code', () => {
-      request(app).get('/feedback').then((response) => {
-        return expect(response.statusCode).toBe(200);
-      });
-    });
+  describe('/scores/new', () => {
+    // test('get requests respond with a 200 status code', () => {
+    //   request(app).get('/feedback').then((response) => {
+    //     return expect(response.statusCode).toBe(200);
+    //   });
+    // });
     test('post requests respond with a 201 status code', (done) => {
       request(app)
-        .post('/feedback')
+        .post('/scores/new')
         .send({ rating: 1, comment: 'Testing' })
         .expect(201, done);
     });
   });
 
-  describe('/confirm', () => {
-    test('responds with a 200 status code', done => {
-      request(app)
-        .get('/confirm')
-        .expect(200, done);
-    });
-  });
+  // describe('/confirm', () => {
+  //   test('responds with a 200 status code', done => {
+  //     request(app)
+  //       .get('/confirm')
+  //       .expect(200, done);
+  //   });
+  // });
 
   describe('all other paths', () => {
     test('returns a 404 status code', () => {
